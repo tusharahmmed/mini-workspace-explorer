e07d14b7fcdd99f60d37f732e18a5464273ee47a
Tushar Ahmmed <sakibahmmedtushar@gmail.com>
2026-09-23 05:23:02 +0600

    implement memory Persistance

--- src/providers/Providers.jsx
+++ src/providers/Providers.jsx
@@ -1,13 +1,16 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { store } from "@/rtk/store";
+import { store, persistor } from "@/rtk/store";
 import { Provider } from "react-redux";
 import { Toaster } from "@/components/ui/toast";
+import { PersistGate } from "redux-persist/integration/react";
 
 const Providers = ({ children }) => {
   return (
     <Provider store={store}>
-      <SidebarProvider>{children}</SidebarProvider>
-      <Toaster />
+      <PersistGate loading={null} persistor={persistor}>
+        <SidebarProvider>{children}</SidebarProvider>
+        <Toaster />
+      </PersistGate>
     </Provider>
   );
 };
--- src/rtk/store.js
+++ src/rtk/store.js
@@ -1,4 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./rootReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({ reducer: rootReducer });
+const persistConfig = {
+  key: "root",
+  storage: storage.default,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
