20ed0dcc5c165b5dc2073bc5cddf0845c3188e3a
Tushar Ahmmed <sakibahmmedtushar@gmail.com>
2026-09-23 02:50:06 +0600

    implemented breadcrumb

--- src/components/custom/CustomeBreakcrumb.jsx
+++ src/components/custom/CustomeBreakcrumb.jsx
@@ -1,27 +1,54 @@
 import {
   Breadcrumb,
   BreadcrumbItem,
-  BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { setCurrentDir } from "@/rtk/features/fileManager/fileManagerSlice";
+
+import { useDispatch, useSelector } from "react-redux";
+
+const generateBreadCrumb = (list, item) => {
+  const arr = [];
+  const breadCrumb = (list, item) => {
+    if (item == null) {
+      return;
+    }
+
+    const parentDoc = list.find((doc) => doc.id == item.parentId);
+    breadCrumb(list, parentDoc);
+    arr.push(item);
+  };
+  breadCrumb(list, item);
+  return arr;
+};
 
 const CustomeBreakcrumb = ({ className }) => {
+  const fileManager = useSelector((state) => state.fileManager);
+
+  const list = generateBreadCrumb(
+    fileManager.documents,
+    fileManager.currentDir,
+  );
+
+  const dispatch = useDispatch();
   return (
     <Breadcrumb className={className}>
       <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/">Home</BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/components">Components</BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-        </BreadcrumbItem>
+        {list?.map((item, idx) => (
+          <>
+            <BreadcrumbItem
+              className="cursor-pointer"
+              onClick={() => {
+                dispatch(setCurrentDir(item));
+              }}
+            >
+              <BreadcrumbPage>{item.name}</BreadcrumbPage>
+            </BreadcrumbItem>
+            {idx != list?.length - 1 && <BreadcrumbSeparator />}
+          </>
+        ))}
       </BreadcrumbList>
     </Breadcrumb>
   );
